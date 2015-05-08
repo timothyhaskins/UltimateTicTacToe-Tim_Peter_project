e484fec88f7be9ec76ce06cca921fd860d82dfe8
timothyhaskins <timothyhaskins@gmail.com>
2015-05-07 20:23:17 -0700

    Messed with Move

--- app/src/main/java/olsonworks/UltimateTicTacToe/MainActivity.java
+++ app/src/main/java/olsonworks/UltimateTicTacToe/MainActivity.java
@@ -1,8 +1,9 @@
 package olsonworks.UltimateTicTacToe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -16,13 +17,15 @@ public class MainActivity extends ActionBarActivity {
 
     public Board gameBoard;
     public Boolean gameOver;
-    private boolean player2Turn = false;
+    private boolean player1Turn = true;
     private boolean firstMove = true;
     private boolean isAny = false;
     private String buttonText;
+    public Move move = new Move();
+
 
     // Temp variable for board
-    public int[] gameMove = new int[4];
+    public int[] tempGameMove = new int[4];
 
 
     @Override
@@ -33,6 +36,9 @@ public class MainActivity extends ActionBarActivity {
         //Make official gameBoard
         final Button newGame = (Button) findViewById(R.id.new_game_button);
         final TextView moveCounter = (TextView) findViewById(R.id.move_counter);
+
+        // Make a new move
+
 
         // Testing out essentially a "New Game" button w/ a fresh board
 
@@ -48,7 +54,7 @@ public class MainActivity extends ActionBarActivity {
                 moveCounter.startAnimation(animAlpha);
                 gameBoard = new Board();
                 gameOver = false;
-                player2Turn = false;
+                player1Turn = false;
                 firstMove = true;
                 resetAllButtons();
             }
@@ -63,78 +69,24 @@ public class MainActivity extends ActionBarActivity {
 
 
 
-     /*   while (!gameOver)
-        {
-            makeRandomMove(gameBoard);
-            logBoard(gameBoard);
-            gameOver = gameBoard.checkWin();
-        }
-        Log.d("GAME MOVE:", "game over");
-        */
-
-    /* get random board move
-    public void makeRandomMove(Board gameBoard) {
-        //do i get to go anywhere?
-
-        if (gameBoard.getNextGame() == -1) {
-            gameBoard.makeMove((int) (Math.random() * 9), (int) (Math.random() * 9), gameBoard.getCurrentPlayer());
-        } else {
-            //or just in a specific game?
-            gameBoard.makeMove(gameBoard.getNextGame(), (int) (Math.random() * 9), gameBoard.getCurrentPlayer());
-        }
-    } */
-
-    public void logBoard(Board board) {
-
-        String output;
-        int[][][][] boardSpace = board.indexBoard();
-
-        Log.d("GAME MOVE:", "new move");
-        Log.d("GAME MOVE:", "-------------");
-        for (int iGameY = 0; iGameY < 3; iGameY++) {
-            for (int iTileY = 0; iTileY < 3; iTileY++) {
-                output = "|";
-                for (int iGameX = 0; iGameX < 3; iGameX++) {
-                    for (int iTileX = 0; iTileX < 3; iTileX++) {
-                        switch (boardSpace[iGameX][iGameY][iTileX][iTileY]) {
-                            case 1:
-                                output += "X";
-                                break;
-                            case 2:
-                                output += "O";
-                                break;
-                            default:
-                                output += "*";
-                                break;
-                        }
-                    }
-                    output += "|";
-                }
-                Log.d("GAME MOVE:", output);
-            }
-            Log.d("GAME MOVE:", "-------------");
-        }
-    }
-
-
     /* Set up these onClickListeners for alllll the gameboard buttons.   Basically it is:
     // Going TableLayout (whole game) -> TableRow (row of subgames)
      -> TableLayout (subgame) -> Table Row (row of buttons) -> Button */
 
     private void setupOnClickListeners() {
         TableLayout T = (TableLayout) findViewById(R.id.totalboard);
-        for (int y2 = 0; y2 < T.getChildCount(); y2++) {
-            if (T.getChildAt(y2) instanceof TableRow) {
-                TableRow R1 = (TableRow) T.getChildAt(y2);
-                for (int x2 = 0; x2 < R1.getChildCount(); x2++) {
-                    if (R1.getChildAt(x2) instanceof TableLayout) {
-                        TableLayout T2 = (TableLayout) R1.getChildAt(x2);
-                        for (int y1 = 0; y1 < T2.getChildCount(); y1++) {
-                            if (T2.getChildAt(y1) instanceof TableRow) {
-                                TableRow R2 = (TableRow) T2.getChildAt(y1);
-                                for (int x1 = 0; x1 < R2.getChildCount(); x1++) {
-                                    View V = R2.getChildAt(x1);
-                                    V.setOnClickListener(new playOnClick(x1, y1, x2, y2));
+        for (int tileY = 0; tileY < T.getChildCount(); tileY++) {
+            if (T.getChildAt(tileY) instanceof TableRow) {
+                TableRow R1 = (TableRow) T.getChildAt(tileY);
+                for (int tileX = 0; tileX < R1.getChildCount(); tileX++) {
+                    if (R1.getChildAt(tileX) instanceof TableLayout) {
+                        TableLayout T2 = (TableLayout) R1.getChildAt(tileX);
+                        for (int gameY = 0; gameY < T2.getChildCount(); gameY++) {
+                            if (T2.getChildAt(gameY) instanceof TableRow) {
+                                TableRow R2 = (TableRow) T2.getChildAt(gameY);
+                                for (int gameX = 0; gameX < R2.getChildCount(); gameX++) {
+                                    View V = R2.getChildAt(gameX);
+                                    V.setOnClickListener(new playOnClick(gameX, gameY, tileX, tileY));
                                 }
                             }
                         }
@@ -187,22 +139,21 @@ public class MainActivity extends ActionBarActivity {
 
     private class playOnClick implements View.OnClickListener {
 
-        private int x1 = 0;
-        private int y1 = 0;
-        private int x2 = 0;
-        private int y2 = 0;
+        private int tileX = 0;
+        private int tileY = 0;
+        private int gameX = 0;
+        private int gameY = 0;
         private TextView moveCounter = (TextView) findViewById(R.id.move_counter);
 
 
         // This grabs the coordinates.
 
-        public playOnClick(int x1, int y1, int x2, int y2) {
-            this.x1 = x1;
-            this.y1 = y1;
-            this.x2 = x2;
-            this.y2 = y2;
+        public playOnClick(int tileX, int tileY, int gameX, int gameY) {
+            this.tileX = tileX;
+            this.tileY = tileY;
+            this.gameX = gameX;
+            this.gameY = gameY;
         }
-
 
         /* This puts the X and O on the button and displays the coordinates of the move.
          It changes the display to X or O depending on whose turn.   Also locks out the button.
@@ -215,19 +166,37 @@ public class MainActivity extends ActionBarActivity {
             final Animation animScale = AnimationUtils.loadAnimation(MainActivity.this, R.anim.scale);
 
             if (view instanceof Button) {
-                gameMove[0] = x1;
-                gameMove[1] = y1;
-                gameMove[2] = x2;
-                gameMove[3] = y2;
+                tempGameMove[0] = tileX;
+                tempGameMove[1] = tileY;
+                tempGameMove[2] = gameX;
+                tempGameMove[3] = gameY;
+                move.setTileX(tileX);
+                move.setTileY(tileY);
+                move.setGameX(gameX);
+                move.setGameY(gameY);
+                move.setPlayer1Turn(player1Turn);
+
+                // Starting to try to use an intent to pass the move to the game - not actually used yet
+                // playGame(move);
+
+                // Grab the button and set it to O or X
+
                 Button B = (Button) view;
-                B.setText(player2Turn ? "O" : "X");
+                B.setText(move.getPlayer1Turn() ? "O" : "X");
+
                 // Two different ways to animate - old:
                 B.startAnimation(animScale);
                 // New:
                 B.animate().rotationYBy(180).setDuration(300);
+
+                // Disable the button
                 B.setEnabled(false);
-                moveCounter.setText(x1 + "," + y1 + "," + x2 + "," + y2);
-                player2Turn = !player2Turn;
+
+                // Display the move
+                moveCounter.setText(move.mTileX + "," + move.mTileY + "," + move.mGameX + "," + move.mGameY);
+
+                // Reverse the player
+                player1Turn = !player1Turn;
 
                 if (firstMove) {
                     disableBoardAfterAny();
@@ -242,14 +211,23 @@ public class MainActivity extends ActionBarActivity {
                 enableNewSubgame();
             }
         }
+    }
 
+
+    // This would pass the move to the game via an intent.   Doesn't work.
+
+    private void playGame(Move newMove) {
+        Intent intent = new Intent(MainActivity.this, Move.class);
+        intent.putExtra("newMove", (Parcelable) newMove);
+        startActivity(intent);
+    }
 
 
 
         public void disableOldSubgame() {
             TableLayout T = (TableLayout) findViewById(R.id.totalboard);
-            TableRow R1 = (TableRow) T.getChildAt(gameMove[3]);
-            TableLayout T2 = (TableLayout) R1.getChildAt(gameMove[2]);
+            TableRow R1 = (TableRow) T.getChildAt(move.getGameY());
+            TableLayout T2 = (TableLayout) R1.getChildAt(move.getGameX());
             for (int y = 0; y < T2.getChildCount(); y++) {
                 if (T2.getChildAt(y) instanceof TableRow) {
                     TableRow R2 = (TableRow) T2.getChildAt(y);
@@ -265,8 +243,8 @@ public class MainActivity extends ActionBarActivity {
 
         public void enableNewSubgame() {
             TableLayout T = (TableLayout) findViewById(R.id.totalboard);
-            TableRow R1 = (TableRow) T.getChildAt(gameMove[1]);
-            TableLayout T2 = (TableLayout) R1.getChildAt(gameMove[0]);
+            TableRow R1 = (TableRow) T.getChildAt(move.getTileY());
+            TableLayout T2 = (TableLayout) R1.getChildAt(move.getTileX());
             for (int y = 0; y < T2.getChildCount(); y++) {
                 if (T2.getChildAt(y) instanceof TableRow) {
                     TableRow R2 = (TableRow) T2.getChildAt(y);
@@ -311,7 +289,7 @@ public class MainActivity extends ActionBarActivity {
             }
         }
     }
-}
+
 
 
 
--- app/src/main/java/olsonworks/UltimateTicTacToe/Move.java
+++ app/src/main/java/olsonworks/UltimateTicTacToe/Move.java
@@ -6,23 +6,64 @@ package olsonworks.UltimateTicTacToe;
 public class Move {
 
     //coordinate values of game move
-    public int gameX;
-    public int gameY;
-    public int tileX;
-    public int tileY;
+    public int mTileX;
+    public int mTileY;
+    public int mGameX;
+    public int mGameY;
+    public boolean mPlayer1Turn;
 
     //creates new empty move
-    public Move(){
-
-    }
+    public Move(){}
 
     //creates a new move from XY coordinates'
-    public Move(int tX, int tY, int gX,int gY){
-        gameX = gX;
-        gameY = gY;
-        tileX = tX;
-        tileY = tY;
+    public Move(int tileX, int tileY, int gameX, int gameY, boolean player1Turn){
+        mTileX = tileX;
+        mTileY = tileY;
+        mGameX = gameX;
+        mGameY = gameY;
+        mPlayer1Turn = !player1Turn;
     }
+
+    public int getTileX() {
+        return mTileX;
+    }
+
+    public void setTileX(int tileX) {
+        mTileX = tileX;
+    }
+
+    public int getTileY() {
+        return mTileY;
+    }
+
+    public void setTileY(int tileY) {
+        mTileY = tileY;
+    }
+
+    public int getGameX() {
+        return mGameX;
+    }
+
+    public void setGameX(int gameX) {
+        mGameX = gameX;
+    }
+
+    public int getGameY() {
+        return mGameY;
+    }
+
+    public void setGameY(int gameY) {
+        mGameY = gameY;
+    }
+
+    public boolean getPlayer1Turn() {
+        return mPlayer1Turn;
+    }
+
+    public void setPlayer1Turn(boolean player1Turn) {
+        mPlayer1Turn = player1Turn;
+    }
+
 
     //creates a new move from combined Integer value'
     public Move(int move){
@@ -30,20 +71,20 @@ public class Move {
         int game = (int)(move/10);
         int tile = (move % 10);
         //parse integers into X and Y coordinates
-        gameX = game % 3;
-        gameY = (int)(game/3);
-        tileX = tile % 3;
-        tileY = (int)(tile/3);
+        mGameX = game % 3;
+        mGameY = (int)(game/3);
+        mTileX = tile % 3;
+        mTileY = (int)(tile/3);
     }
 
-    //returns the moves in an integer array with the tile and game coordinates in XY format (tileX,tileY,gameX.gameY format).
+    //returns the moves in an integer array with the tile and game coordinates in XY format (mTileX,mTileY,mGameX.mGameY format).
     public int[] getMoveArray(){
-        return new int[]{tileX,tileY,gameX,gameY};
+        return new int[]{mTileX, mTileY, mGameX, mGameY};
     }
 
     //returns the move as a 2 digit integer (for use with old system of integers. will be depreciated I think
     public int getMoveInteger(){
-        return ((((gameY*3)+gameX)*10) + ((tileY*3)+tileX));
+        return ((((mGameY *3)+ mGameX)*10) + ((mTileY *3)+ mTileX));
     }
 
 }
